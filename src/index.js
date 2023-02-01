b835e51ec8b9eeb221cee63e92a94576d1f9873a
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-01 17:38:42 -0600

    created and appended list entries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+//function needs to
+// create a new li
+//fill it with the submission text content
+//append it to the correct ul
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let thisForm = document.querySelector('form');
+  thisForm.addEventListener('submit', handleLi)
 });
+
+function handleLi (eventObj) {
+  eventObj.preventDefault();
+  let newLi = document.createElement('li');
+  newLi.textContent = eventObj.target['new-task-description'].value;
+  document.querySelector('#tasks').append(newLi);
+};
+
+//Questions 
+// How exactly does .target work?
+// Practice using the debugging tool
+// How quickly does the learning escalate?
+// Practice Labs for tonight?
+//
+
